9fd067632a3c4003d713b4cf03fd5e389822d4a2
msaspence <msaspence@gmail.com>
2022-10-14 10:01:34 +0100

    Add build scripts

---
+++ packages/db/src/knexconfig.ts
@@ -0,0 +1,45 @@
+module.exports = {
+  timezone: 'UTC',
+  client: 'postgresql',
+  connection: {
+    database: 'hearye',
+  },
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'hearye',
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
+  },
+}
