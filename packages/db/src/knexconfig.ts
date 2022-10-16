3c5ec8f2d509e1fba93da4ec1b0eecb3c61a0df9
msaspence <msaspence@gmail.com>
2022-10-16 17:25:20 +0100

    More ts-node

--- packages/db/src/knexconfig.ts
+++ packages/db/src/knexconfig.ts
@@ -1,5 +1,6 @@
 import type { Knex } from 'knex'
 import '@hearye/env'
+import { xxx } from 'xxx'
 
 const config: Knex.Config | { [key: string]: Knex.Config } = {
   client: 'postgresql',
@@ -13,5 +14,5 @@ const config: Knex.Config | { [key: string]: Knex.Config } = {
     max: 10,
   },
 }
-
+console.log(xxx)
 module.exports = config
