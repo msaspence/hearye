16e12bbb9bfc657a92f7f9b121f616e222e8defb
msaspence <msaspence@gmail.com>
2022-11-02 08:33:48 +0000

    Fav icons

--- apps/site/vite.config.ts
+++ apps/site/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), ViteFaviconsPlugin('src/images/icon.png')],
 })
