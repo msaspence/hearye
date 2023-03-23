35e14548c31b5f0872abe3522f399aafc83d8ce2
msaspence <msaspence@gmail.com>
2023-03-23 08:24:11 +0000

    Add Google Analytics

--- apps/site/src/renderer/_default.page.server.tsx
+++ apps/site/src/renderer/_default.page.server.tsx
@@ -16,11 +16,20 @@ export async function render({ Page }: PageContextBuiltIn) {
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html>
       <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XS0B008NL4"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XS0B008NL4');
+        </script>
         <title>Hear Ye! - Slack message acknowledgements and reminders</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:900|Lato:700|Lato:400">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Just+Another+Hand:400">
         ${dangerouslySkipEscape(favIconHtml.join('\n'))}
         <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        
       </head>
       <body>
         <div id="page-root">${dangerouslySkipEscape(html)}</div>
