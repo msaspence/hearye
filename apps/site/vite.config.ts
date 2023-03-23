750a6e2af3bc6480e0136d18dc7171705fbdeb3c
msaspence <msaspence@gmail.com>
2023-03-23 15:59:42 +0000

    Add tsconfigPaths

--- apps/site/vite.config.ts
+++ apps/site/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 import swc from 'unplugin-swc'
 import { cjsInterop } from 'vite-plugin-cjs-interop'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     react(),
     ssr({ prerender: true }),
     cjsInterop({
