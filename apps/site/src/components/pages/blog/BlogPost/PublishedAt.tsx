4fc542b02394eceee6c904380288438278014e0d
msaspence <msaspence@gmail.com>
2023-04-11 09:36:09 +0100

    Fix published at

--- apps/site/src/components/pages/blog/BlogPost/PublishedAt.tsx
+++ apps/site/src/components/pages/blog/BlogPost/PublishedAt.tsx
@@ -1,9 +1,7 @@
 import Typography from '@mui/material/Typography'
 
-console.log('</Typography>', Typography)
-// import { formatDate, Dayjs } from '@hearye/dayjs'
+import { formatDate, Dayjs } from '@hearye/dayjs'
 
 export function PublishedAt({ at }: { at: Dayjs }) {
-  return <Typography>hello</Typography>
-  // return <Typography sx={{ color: '#777' }}>{formatDate(at)}</Typography>
+  return <Typography sx={{ color: '#777' }}>{formatDate(at)}</Typography>
 }
