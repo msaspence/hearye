43d36eefbf35ef98a87525c9378fd0c0fbd20f22
msaspence <msaspence@gmail.com>
2023-04-17 15:58:14 +0100

    Add social profile icons

--- apps/site/src/components/Layout/Footer/index.tsx
+++ apps/site/src/components/Layout/Footer/index.tsx
@@ -1,6 +1,12 @@
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { List, ListItem } from './List'
+import {
+  Twitter as TwitterIcon,
+  LinkedIn as LinkedInIcon,
+  Facebook as FacebookIcon,
+  Email as EmailIcon,
+} from '@mui/icons-material'
 
 export function Footer() {
   return (
@@ -16,12 +22,18 @@ export function Footer() {
       </Typography>
       <Box sx={{ flexGrow: 1 }} />
       <List sx={{ marginRight: 4 }}>
-        <ListItem href="https://twitter.com/HearYeBot">Twitter</ListItem>
-        <ListItem href="https://www.linkedin.com/company/hear-ye-bot">
-          LinkedIn
+        <ListItem href="https://twitter.com/HearYeBot">
+          <TwitterIcon /> Twitter
         </ListItem>
-        <ListItem href="https://www.facebook.com/HearYeBot">Facebook</ListItem>
-        <ListItem href="mailto:hello@hearyebot.com">Contact Us</ListItem>
+        <ListItem href="https://www.linkedin.com/company/hear-ye-bot">
+          <LinkedInIcon /> LinkedIn
+        </ListItem>
+        <ListItem href="https://www.facebook.com/HearYeBot">
+          <FacebookIcon /> Facebook
+        </ListItem>
+        <ListItem href="mailto:hello@hearyebot.com">
+          <EmailIcon /> Contact Us
+        </ListItem>
       </List>
       <List>
         <ListItem href="/#features">Features</ListItem>
